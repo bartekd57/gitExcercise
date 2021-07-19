2337939d266dd2ad8a65e5c914803beeb105f516
Bartek Domanski <bartosz2.domanski@orange.com>
2021-07-19 17:01:44 +0200

    zonda cinque commit

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         String car4 = "Skoda Superb gen 3";
         String car5 = "Skoda Fabia";
         String car6 = "Ford2";
-        String car7 = "Pagani Zonda";
+        String car7 = "Pagani Zonda Cinque";
         String car8 = "BMW";
 
     }
