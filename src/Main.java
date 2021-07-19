e4497c27b1242620e68fdfc43cb1a908d8d75c78
Bartek Domanski <bartosz2.domanski@orange.com>
2021-07-19 16:21:37 +0200

    ford 2 commit

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         String car3= "Mercedes C63 AMG";
         String car4 = "Skoda Superb gen 3";
         String car5 = "Skoda Fabia";
-        String car6 = "Ford";
+
 
     }
 }
