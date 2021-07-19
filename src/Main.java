d782be0281233ca64ef25a7efa46499badd0c0bf
Bartek Domanski <bartosz2.domanski@orange.com>
2021-07-19 15:25:27 +0200

    skoda changes combined

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,8 @@ public class Main {
         String car = "Audi";
         String car2 = "Seat";
         String car3= "Mercedes";
-        String car4 = "Skoda Superb";
+        String car4 = "Skoda Superb gen 3";
+        String car5 = "Skoda Fabia";
 
 
     }
